2c2d20aea4bbf4820313268591518447a3446f46
unknown <arslanrehmani@gmail.com>
2026-08-06 17:07:35 +0500

    fix: correct XML namespace URL to sitemaps.org in generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -41,7 +41,7 @@ function generateSitemap() {
   console.log(`📦 Found ${toolList.length} unique tools in toolsCatalog.ts`);
 
   let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-  xml += `<urlset xmlns="http://www.sitemap.org/schemas/sitemap/0.9">\n`;
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
   // 1. Add static pages
   STATIC_ROUTES.forEach((route) => {
